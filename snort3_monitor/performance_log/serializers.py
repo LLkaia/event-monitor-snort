e4e95a5e9759118dbaed70f548eecec3c9743aad
Illia Kaialainien <illia.kaialainien@gmail.com>
2024-01-10 17:48:01 +0200

    redisigned performance results according to new info

--- snort3_monitor/performance_log/serializers.py
+++ snort3_monitor/performance_log/serializers.py
@@ -2,5 +2,11 @@ from rest_framework import serializers
 
 
 class PerformanceSerializer(serializers.Serializer):
+    timestamp = serializers.DateTimeField()
+    module = serializers.CharField(max_length=128)
+    pegcounts = serializers.JSONField()
+
+
+class PerformanceDeltaSerializer(serializers.Serializer):
     module = serializers.CharField(max_length=128)
     pegcounts = serializers.JSONField()
